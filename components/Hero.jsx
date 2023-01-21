b9080b89ab1591c353be884599c6572369c830da
Natuza <natuza.machado@gmail.com>
2023-01-20 23:47:38 -0500

    fix css display

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -7,7 +7,7 @@ function Hero(){
         <img alt= "happy photos" src ={groupPhoto}></img>
           <div className="description">
             <h1>Online Experiences</h1>
-            <p>Join unique interactive activities led by, one-of-a-kind hosts - all whithout leaving home.</p>
+            <p>Join unique interactive activities led by one-of-a-kind hosts - all whithout leaving home.</p>
         </div>
     </section>
   )
